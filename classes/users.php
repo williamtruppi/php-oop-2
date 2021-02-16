fde9424e2c6b680d3f0e4cb4304c293b75b24606
William Truppi <williamt@hotmail.it>
2021-02-16 16:03:35 +0100

    aggiunti più attributi e relativi metodi get e set

--- classes/users.php
+++ classes/users.php
@@ -2,14 +2,18 @@
 
  class Users {
    public $username;
+   public $name;
+   public $lastname;
    public $password;
    public $email;
    public $img_avatar;
    public $description;
 
-  public function __construct(string $username, string $password, string $email, string $img_avatar, string $description)
+  public function __construct(string $username, string $name, string $lastname, string $password, string $email, string $img_avatar, string $description)
   {
     $this->username = $username;
+    $this->name = $name;
+    $this->lastname = $lastname;
     $this->password = $password;
     $this->email = $email;
     $this->img_avatar = $img_avatar;
@@ -121,6 +125,46 @@
 
    public function logOut() {}
 
+
+   /**
+    * Get the value of name
+    */ 
+   public function getName()
+   {
+      return $this->name;
+   }
+
+   /**
+    * Set the value of name
+    *
+    * @return  self
+    */ 
+   public function setName($name)
+   {
+      $this->name = $name;
+
+      return $this;
+   }
+
+   /**
+    * Get the value of lastname
+    */ 
+   public function getLastname()
+   {
+      return $this->lastname;
+   }
+
+   /**
+    * Set the value of lastname
+    *
+    * @return  self
+    */ 
+   public function setLastname($lastname)
+   {
+      $this->lastname = $lastname;
+
+      return $this;
+   }
  }
 
 ?>
--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 include __DIR__ . "/classes/users.php";
 
 $users = [
-  new Users("william92", "lollo987", "william@gmail.com", "/img/avatar.jpg", "sono un nuovo utente"),
-  new Users("gino74", "lollo987", "gino74@gmail.com", "/img/avatar.jpg", "sono un utente intermedio"),
-  new Users("francesca99", "lollo987", "francesca99@gmail.com", "/img/avatar.jpg", "sono un nuovo utente"),
+  new Users("william92", "william", "walker", "lollo987", "william@gmail.com", "/img/avatar.jpg", "sono un nuovo utente"),
+  new Users("gino74", "gino", "lollobrigido", "lollo987", "gino74@gmail.com", "/img/avatar.jpg", "sono un utente intermedio"),
+  new Users("francesca99", "francesca", "checca", "lollo987", "francesca99@gmail.com", "/img/avatar.jpg", "sono un nuovo utente"),
 ];
 
 
@@ -25,6 +25,8 @@ $users = [
 
   <?php foreach ($users as $user) { ?>
     <h1>Username --> <?php echo $user->username ?></h1>
+    <h3>Name --> <?php echo $user->name ?></h3>
+    <h3>Lastname --> <?php echo $user->lastname ?></h3>
     <h3>Password --> <?php echo $user->password ?></h3>
     <h3>Email --> <?php echo $user->email ?></h3>
     <h3>Description --> <?php echo $user->description ?></h3>
